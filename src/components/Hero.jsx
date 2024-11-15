044faa293241ee7d6102d071cb2bb6f7fb0f16b3
Mpdanish <danishmaliyekkal786@gmail.com>
2024-11-15 08:32:40 +0530

    video added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,27 @@
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
-import { heroVideo, smallHeroVideo } from '../utils';
+// import { heroVideo, smallHeroVideo } from '../utils';
 import { useEffect, useState } from 'react';
+import videoSrc from '../../public/assets/videos/hero video.mov'
 
 const Hero = () => {
-  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+  // const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
 
-  const handleVideoSrcSet = () => {
-    if(window.innerWidth < 760) {
-      setVideoSrc(smallHeroVideo)
-    } else {
-      setVideoSrc(heroVideo)
-    }
-  }
+  // const handleVideoSrcSet = () => {
+  //   if(window.innerWidth < 760) {
+  //     setVideoSrc(smallHeroVideo)
+  //   } else {
+  //     setVideoSrc(heroVideo)
+  //   }
+  // }
 
-  useEffect(() => {
-    window.addEventListener('resize', handleVideoSrcSet);
+  // useEffect(() => {
+  //   window.addEventListener('resize', handleVideoSrcSet);
 
-    return () => {
-      window.removeEventListener('reisze', handleVideoSrcSet)
-    }
-  }, [])
+  //   return () => {
+  //     window.removeEventListener('reisze', handleVideoSrcSet)
+  //   }
+  // }, [])
 
   useGSAP(() => {
     gsap.to('#hero', { opacity: 1, delay: 2 })
@@ -28,23 +29,20 @@ const Hero = () => {
   }, [])
 
   return (
-    <section className="w-full nav-height bg-black relative">
+    <section className="w-full nav-height bg-black relative background">
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">ZEECODE SOLUTIONS</p>
         <div className="md:w-10/12 w-9/12">
-          <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
-            <source src={videoSrc} type="video/mp4" />
-          </video>
-        </div>
-      </div>
-
-      <div
+          <div
         id="cta"
         className="flex flex-col items-center opacity-0 translate-y-20"
       >
-        <a href="#highlights" className="btn">Buy</a>
-        <p className="font-normal text-xl">From $199/month or $999</p>
+        <a href="#" className="btn">Know More</a>
       </div>
+        </div>
+      </div>
+
+      
     </section>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import { appleImg, bagImg, searchImg } from '../utils';
 import { navLists } from '../constants';
-import logo from "../../public/assets/images/new logo.png"
+import logo from "../../public/assets/images/logo-removebg-preview.png"
 
 const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
-        <img src={logo} alt="ZC" width={160} height={200} />
+        <img src={logo} alt="ZC" width={50} height={50} />
 
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
@@ -16,10 +16,10 @@ const Navbar = () => {
           ))}
         </div>
 
-        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+        {/* <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
           <img src={searchImg} alt="search" width={18} height={18} />
           <img src={bagImg} alt="bag" width={18} height={18} />
-        </div>
+        </div> */}
       </nav>
     </header>
   )
