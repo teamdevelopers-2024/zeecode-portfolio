60370f2e6b115805adb94cd0b8f7ac2c3ee1d132
Mpdanish <danishmaliyekkal786@gmail.com>
2024-11-15 08:33:47 +0530

    video added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,8 @@
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 // import { heroVideo, smallHeroVideo } from '../utils';
-import { useEffect, useState } from 'react';
-import videoSrc from '../../public/assets/videos/hero video.mov'
+import { useEffect, useState } from "react";
+import videoSrc from "../../public/assets/videos/hero video.mov";
 
 const Hero = () => {
   // const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
@@ -24,27 +24,29 @@ const Hero = () => {
   // }, [])
 
   useGSAP(() => {
-    gsap.to('#hero', { opacity: 1, delay: 2 })
-    gsap.to('#cta', { opacity: 1, y: -50, delay: 2 })
-  }, [])
+    gsap.to("#hero", { opacity: 1, delay: 2 });
+    gsap.to("#cta", { opacity: 1, y: -50, delay: 2 });
+  }, []);
 
   return (
     <section className="w-full nav-height bg-black relative background">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p id="hero" className="hero-title">ZEECODE SOLUTIONS</p>
+        <p id="hero" className="hero-title">
+          ZEECODE SOLUTIONS
+        </p>
         <div className="md:w-10/12 w-9/12">
           <div
-        id="cta"
-        className="flex flex-col items-center opacity-0 translate-y-20"
-      >
-        <a href="#" className="btn">Know More</a>
-      </div>
+            id="cta"
+            className="flex flex-col items-center opacity-0 translate-y-20"
+          >
+            <a href="#" className="btn">
+              Know More
+            </a>
+          </div>
         </div>
       </div>
-
-      
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
