d0a47ab3996bd3e03291b4cabe193c9c42ab41cf
shaheen5115 <ahammedshahmkd5115.com>
2024-11-20 16:58:56 +0530

    about us done

---
+++ public/assets/index.js
@@ -0,0 +1,81 @@
+import benefitIcon1 from '../assets/images/services/icon-1.svg'
+import benefitIcon2 from '../assets/images/services/icon-2.svg'
+import benefitIcon3 from '../assets/images/services/icon-3.svg'
+import benefitIcon4 from '../assets/images/services/icon-4.svg'
+import card1 from "../assets/images/services/card-1.svg"
+import card2 from "../assets/images/services/card-2.svg"
+import card3 from "../assets/images/services/card-3.svg"
+import card4 from "../assets/images/services/card-4.svg"
+import card5 from "../assets/images/services/card-5.svg"
+import card6 from "../assets/images/services/card-6.svg"
+import benefitImage2 from "../assets/images/services/image-2.jpg"
+
+
+
+
+export const benefits = [
+    {
+      id: "0",
+      title: "Web Development",
+      text: "Building seamless, innovative websites with Code&Click. Let’s turn your online presence into a powerful experience!",
+      backgroundUrl: card1,
+      iconUrl: benefitIcon1,
+      imageUrl: benefitImage2,
+    },
+    {
+      id: "1",
+      title: "App Development",
+      text: "Crafting innovative apps that bring your vision to life with precision and efficiency.",
+      backgroundUrl: card2,
+      iconUrl: benefitIcon2,
+      imageUrl: benefitImage2,
+      light: true,
+    },
+    {
+      id: "2",
+      title: "Digital Marketing",
+      text: "Elevate your brand with tailored digital marketing strategies that drive results and maximize your online presence.",
+      backgroundUrl: card3,
+      iconUrl: benefitIcon3,
+      imageUrl: benefitImage2,
+    },
+    {
+      id: "3",
+      title: "Job Finder",
+      text: "Connecting top talent with the perfect opportunities. Find your next job with ease and precision.",
+      backgroundUrl: card4,
+      iconUrl: benefitIcon4,
+      imageUrl: benefitImage2,
+      light: true,
+    },
+    {
+      id: "4",
+      title: "Start-up Business Strategy",
+      text: "Empowering startups with strategic insights and tailored solutions to drive growth and success.",
+      backgroundUrl: card5,
+      iconUrl: benefitIcon1,
+      imageUrl: benefitImage2,
+    },
+    {
+      id: "5",
+      title: "CRM Development",
+      text: "Amplify your brand’s reach with targeted LinkedIn campaigns that drive engagement and build connections.",
+      backgroundUrl: card6,
+      iconUrl: benefitIcon2,
+      imageUrl: benefitImage2,
+    },
+  ];
+
+export{
+    card1,
+    card2,
+    card3,
+    card4,
+    card5,
+    card6,
+    benefitIcon1,
+    benefitIcon2,
+    benefitIcon3,
+    benefitIcon4,
+    benefitImage2
+}
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,95 +1,117 @@
-import { useGSAP } from '@gsap/react'
-import React, { useRef } from 'react'
-import { animateWithGsap } from '../utils/animations';
-import { explore1Img, explore2Img, exploreVideo } from '../utils';
-import gsap from 'gsap';
+import { useGSAP } from "@gsap/react";
+import React, { useRef } from "react";
+import { animateWithGsap } from "../utils/animations";
+import { explore1Img, explore2Img, exploreVideo } from "../utils";
+import { benefits } from "../constants";
+import gsap from "gsap";
+import Arrow from "../../public/assets/images/services/Arrow";
+import ClipPath from "../../public/assets/images/services/ClipPath";
+import GradientLight from "../../public/assets/images/services/Benefits";
 
 const Features = () => {
   const videoRef = useRef();
 
   useGSAP(() => {
-    gsap.to('#exploreVideo', {
+    gsap.to("#exploreVideo", {
       scrollTrigger: {
-        trigger: '#exploreVideo',
-        toggleActions: 'play pause reverse restart',
-        start: '-10% bottom',
+        trigger: "#exploreVideo",
+        toggleActions: "play pause reverse restart",
+        start: "-10% bottom",
       },
       onComplete: () => {
         videoRef.current.play();
-      }
-    })
+      },
+    });
 
-    animateWithGsap('#features_title', { y:0, opacity:1})
+    animateWithGsap("#features_title", { y: 0, opacity: 1 });
     animateWithGsap(
-      '.g_grow',
-      { scale: 1, opacity: 1, ease: 'power1' },
+      ".g_grow",
+      { scale: 1, opacity: 1, ease: "power1" },
       { scrub: 5.5 }
     );
-    animateWithGsap(
-      '.g_text',
-      {y:0, opacity: 1,ease: 'power2.inOut',duration: 1}
-    )
+    animateWithGsap(".g_text", {
+      y: 0,
+      opacity: 1,
+      ease: "power2.inOut",
+      duration: 1,
+    });
   }, []);
 
   return (
-    <section className="h-full common-padding bg-zinc relative overflow-hidden">
+    <section id="services" className="h-full common-padding bg-zinc relative overflow-hidden">
       <div className="screen-max-wdith">
-        <div className="mb-12 w-full">
-          <h1 id="features_title" className="section-heading">Explore the full story.</h1>
+        <div className="mb-12 ml-10 w-full">
+          <h1 id="features_title" className="section-heading">
+          What We Deliver
+          </h1>
         </div>
-        
+
         <div className="flex flex-col justify-center items-center overflow-hidden">
-          <div className="mt-32 mb-24 pl-24">
-            <h2 className="text-5xl lg:text-7xl font-semibold">iPhone.</h2>
-            <h2 className="text-5xl lg:text-7xl font-semibold">Forged in titanium.</h2>
-          </div>
+          <div className="mt-32 mb-24 pl-24"></div>
 
           <div className="flex-center flex-col sm:px-10">
             <div className="relative h-[50vh] w-full flex items-center">
-              <video playsInline id="exploreVideo" className="w-full h-full object-cover object-center" preload="none" muted autoPlay ref={videoRef}>
-                <source src={exploreVideo} type="video/mp4" />
-              </video>
+              <div className="flex flex-wrap gap-10 mb-15">
+                {benefits.map((item) => (
+                  <div
+                    className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+                    style={{
+                      backgroundImage: `url(${item.backgroundUrl})`,
+                    }}
+                    key={item.id}
+                  >
+                    <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                      <h5 className="h5 mb-5">{item.title}</h5>
+                      <p className="body-2 mb-6 text-n-3">{item.text}</p>
+                      <div className="flex items-center mt-auto">
+                        <img
+                          src={item.iconUrl}
+                          width={48}
+                          height={48}
+                          alt={item.title}
+                        />
+                        <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                          Get service
+                        </p>
+                        <Arrow />
+                      </div>
+                    </div>
+
+                    {item.light && <GradientLight />}
+
+                    <div
+                      className="absolute inset-0.5 bg-n-8"
+                      style={{ clipPath: "url(#benefits)" }}
+                    >
+                      <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                  {item.imageUrl && (
+                    <img
+                      src={item.imageUrl}
+                      width={380}
+                      height={362}
+                      alt={item.title}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
+                </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
 
             <div className="flex flex-col w-full relative">
               <div className="feature-video-container">
-                <div className="overflow-hidden flex-1 h-[50vh]">
-                  <img src={explore1Img} alt="titanium" className="feature-video g_grow" />
-                </div>
-                <div className="overflow-hidden flex-1 h-[50vh]">
-                  <img src={explore2Img} alt="titanium 2" className="feature-video g_grow" />
-                </div>
+                <div className="overflow-hidden flex-1 h-[50vh]"></div>
               </div>
 
-              <div className="feature-text-container">
-                <div className="flex-1 flex-center">
-                  <p className="feature-text g_text">
-                    iPhone 15 Pro is {' '}
-                    <span className="text-white">
-                      the first iPhone to feature an aerospace-grade titanium design
-                    </span>,
-                    using the same alloy that spacecrafts use for missions to Mars.
-                  </p>
-                </div>
-
-                <div className="flex-1 flex-center">
-                  <p className="feature-text g_text">
-                    Titanium has one of the best strength-to-weight ratios of any metal, making these our {' '}
-                    <span className="text-white">
-                      lightest Pro models ever.
-                    </span>
-                    You'll notice the difference the moment you pick one up.
-                  </p>
-                </div>
-
-
-              </div>
+              <div className="feature-text-container"></div>
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
